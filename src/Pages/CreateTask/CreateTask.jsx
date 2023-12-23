00e6e1127c46e08053d1d6727f3a9f40de5a760d
FahimHasan683031 <fahimhasan683031@gmail.com>
2023-12-23 16:30:22 +0600

    add task create page and form

--- src/Pages/CreateTask/CreateTask.jsx
+++ src/Pages/CreateTask/CreateTask.jsx
@@ -16,20 +16,16 @@ const CreateTask = () => {
     const formHandle = e => {
         e.preventDefault()
         const form = e.target;
-        const serviceName = form.serviceName.value;
-        const serviceImage = form.serviceImage.value;
-        const providerName = form.providerName.value;
-        const email = form.email.value;
-        const price = form.price.value;
-        const area = form.area.value;
-        const providerDescription = form.providerDescription.value;
-        const serviceDescription = form.serviceDescription.value;
-        const providerImage=user.photoURL;
+        const taskTitle = form.taskTitle.value;
+        const date = form.date.value;
+        const time = form.time.value;
+        const priority = form.priority.value;
+        const taskDescription = form.taskDescription.value;
+        const email=user?.email
 
-        const service = {serviceName,serviceImage,providerName,email,price,area,providerDescription,serviceDescription,providerImage }
+        const task = {taskTitle,date,time,priority,taskDescription,email }
 
-
-        axiosSecure.post('/api/v1/services',service)
+        axiosSecure.post('/tasks',task)
             .then(data => {
                 if (data.data.insertedId) {
                     toast.success('Successfully Add Service!')
@@ -41,64 +37,51 @@ const CreateTask = () => {
     }
     return (
         <div className="max-w-screen-xl mx-auto px-6">
-            <form className="max-w-3xl mx-auto bg-teal-100 drop-shadow-lg my-24 px-6 py-8 rounded-lg text-base text-black" onSubmit={formHandle} action="">
+            <form className="max-w-md mx-auto bg-teal-100 drop-shadow-lg my-24 px-6 py-8 rounded-lg text-base text-black" onSubmit={formHandle} action="">
 
-                <h1 className="text-center text-teal-600 mb-12 md:text-5xl text-3xl font-bold">Add Service</h1>
+                <h1 className="text-center text-teal-600 mb-12 md:text-5xl text-3xl font-bold">Create Task</h1>
 
-                <div className="flex flex-col md:flex-row gap-5">
+                
                     <div className=" w-full">
                         <label>
-                            Service Name
+                            Task Title
                         </label>
-                        <input required className="w-full mt-1 px-4 py-2 drop-shadow-lg mb-4 rounded  " placeholder="Enter Service Name" type="text" name="serviceName" id="" />
+                        <input required className="w-full mt-1 px-4 py-2 drop-shadow-lg mb-4 rounded  " placeholder="Enter Service Name" type="text" name="taskTitle" id="" />
                     </div>
-
-                    <div className="w-full">
-                        <label >
-                            Service Image URL
-                        </label>
-                        <input required className="px-4 mt-1 w-full py-2 drop-shadow-lg rounded mb-4" placeholder="Enter Service Image url" type="url" name="serviceImage" id="" />
-
-                    </div>
-                </div>
-
-                <div className="flex flex-col md:flex-row gap-5">
                     <div className=" w-full">
                         <label>
-                            Your Name
+                            Select Priority
                         </label>
-                        <input type="text" className="px-4 mt-1 w-full py-2 drop-shadow-lg rounded mb-4" name="providerName" value={user?.displayName} readOnly id="" />
+                        <select  name="priority" required defaultValue='deafult' className="w-full mt-1 px-4 py-2 drop-shadow-lg mb-4 rounded  "  id="">
+                            <option disabled value='deafult'>Select Priority</option>
+                            <option value="Low">Low</option>
+                            <option value="Medium">Medium</option>
+                            <option value="Heigh">Heigh</option>
+                        </select>
                     </div>
 
-                    <div className="w-full">
-                        <label >
-                            Your Email
-                        </label>
-                        <input className="px-4 mt-1 w-full py-2 drop-shadow-lg rounded mb-4" type="email" name="email" value={user?.email} id="" />
-                    </div>
-                </div>
-
-                <div className="flex flex-col md:flex-row gap-5">
+                    <div className="flex gap-4">
                     <div className=" w-full">
                         <label>
-                            price
+                            Select Date
                         </label>
-                        <input required className="w-full mt-1 px-4 py-2 drop-shadow-lg mb-4 rounded  " type="text" name="price" placeholder="Enter Service Price" id="" />
+                        <input  name="date" required className="w-full mt-1 px-4 py-2 drop-shadow-lg mb-4 rounded  " type="date"  id="" />
+                    </div>
+                    <div className=" w-full">
+                        <label>
+                            Select Time
+                        </label>
+                        <input  name="time" required className="w-full mt-1 px-4 py-2 drop-shadow-lg mb-4 rounded  " type="time" id="" />
+                    </div>
                     </div>
 
-                    <div className="w-full">
-                        <label >
-                            Service Area
-                        </label>
-                        <input required className="px-4 mt-1 w-full py-2 drop-shadow-lg rounded mb-4" type="text" name="area" placeholder="Enter Service Area" id="" />
-                    </div>
-                </div>
-                <div className="flex flex-col md:flex-row  gap-5">
-                    <textarea required className=" my-3 md:my-6 drop-shadow-lg p-3 rounded flex-1 " name="serviceDescription" placeholder="Enter Service Description" id="" rows="5"></textarea>
-                    <textarea required className="my-3 md:my-6 drop-shadow-lg p-3 rounded flex-1" name="providerDescription" placeholder="Enter provider Description" id=""  rows="5"></textarea>
-                </div>
+                   
 
-                <input className="w-full font-bold my-5 cursor-pointer py-3 rounded-md text-white bg-teal-600" type="submit" value="Submit" />
+                
+                    <textarea required className=" my-3 w-full md:my-6 drop-shadow-lg p-3 rounded flex-1 " name="taskDescription" placeholder="Enter Task Description" id="" rows="5"></textarea>
+                  
+
+                <input className="w-full font-bold my-3 cursor-pointer py-3 rounded-md text-white bg-teal-600" type="submit" value="Create" />
             </form>
 
         </div>
